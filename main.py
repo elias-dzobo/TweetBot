f4a231b0c2836af78f851ed68b468326a89cf89f
elias-dzobo <elias.dzobo@gmail.com>
2021-07-25 05:07:00 +0000

    Tokenize script added

--- main.py
+++ main.py
@@ -2,24 +2,33 @@ import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+import time
 
 
 consumer_key = 'IjnmXY2vpjAJJek8dWM51YiHF'
 consumer_secret = 'NP5985N98dy7AWqI0elUAnpHgRzKj9sGVki8qjnFnIqrlCUCx8'
 access_token  = '948717579224473600-bRNh6cINiDp4sKIRX8Dip4LCJJF42di'
 access_secret = 'P7JY1PIvEjYVB9fXSSYyP0HCkiWU37B7k91Ciz0f8nXZq'
+start_time = time.time()
+keyword_list = ['$btc', '$ada', '$matic', '$eth']
 
 
 #create stream class
 class MyListener(StreamListener):
 
+    def __init__(self, start_time, time_limit = 60):
+        self.time = start_time
+        self.limit = time_limit
+
     def on_data(self, data):
         try:
             with open('ada.json', 'a') as f:
-                f.write(data)
-                return True
+                while (time.time() - self.time) < self.limit:
+                    f.write(data)
+                    return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
+            time.sleep(5)
         return True
 
     def on_error(self, status):
@@ -33,5 +42,5 @@ if __name__ == '__main__':
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
-    twitter_stream = Stream(auth, MyListener())
-    twitter_stream.filter(track = ['$btc', '$ada', '$matic', '$eth'])
+    twitter_stream = Stream(auth, MyListener(start_time, time_limit=60))
+    twitter_stream.filter(track = keyword_list, languages=['en'])
---
+++ preprocess.py
@@ -0,0 +1,45 @@
+import json
+from nltk.tokenize import word_tokenize
+import re
+
+
+# tokenize the tweet
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+ 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+
+# check tweet
+with open('ada.json', 'r') as f:
+    line = f.readline()  # read first line
+    tweet = json.loads(line) #load line as a dict
+    tokens = preprocess(tweet['text'])
+    print(tokens)
